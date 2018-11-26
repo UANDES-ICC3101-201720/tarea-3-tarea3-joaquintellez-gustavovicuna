e344f6dea1268b0d834e97ae905b68bbf8cba8c7
Gustavo Vicuña <gpvicuna@miuandes.cl>
2018-11-26 01:11:14 -0300

    Start server and read message

--- client.py
+++ client.py
@@ -11,8 +11,8 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 54321               # Reserve a port for your service.
 
 s.connect((host, port))
 print s.recv(1024)
-s.close                     # Close the socket when done
+s.send('1,1,0,hola')
--- server.py
+++ server.py
@@ -5,16 +5,53 @@ Created on Sun Nov 25 22:07:48 2018
 @author: gus19
 """
 
-import socket               # Import socket module
+import socket
+import sys
+import json
+import traceback
+    
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+def Start_Server():
+    
+    print 'Starting Server'
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 54321
+    try:
+        print 'Binding to the port',port
+        s.bind((host, port))
+    except:
+        print("Bind failed. Error: " + str(sys.exc_info()))
+        sys.exit()
+    s.listen(5)
+    print 'Server now listening...'
+    
+    # Conectando con cliente:
+    while True: 
+       c, addr = s.accept()
+       ip, port2 = str(addr[0]), str(addr[1])
+       print 'Got connection from', ip,'at port',port2
+       c.send('Thank you for connecting')
+       
+       # Leyendo el mensaje:
+       mesage= c.recv(1024)
+       mesage= mesage.split(',')
+       syn, ack, modo, archivo= int(mesage[0]), int(mesage[1]), int(mesage[2]), mesage[3]
+       
+       #Accion dependiendo del mensaje:
+       #Si es un archivo:
+       if (modo==0):
+           # Verificando handshake:
+              if (syn==1):
+                  if (ack==0):
+                      print 'Error with handshake. Sending message again.'
+                  else:
+                      syn=0
+                      ack=0
+       print syn, ack, modo, archivo
+                  
 
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+
+#Main:
+
+Start_Server()
