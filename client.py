bc35fbef074c9ee40bd112629dcaee1ad88e0182
Gustavo Vicuña <gpvicuna@miuandes.cl>
2018-11-25 22:44:18 -0300

    Basic start

--- client.py
+++ client.py
@@ -5,3 +5,14 @@ Created on Sun Nov 25 22:08:34 2018
 @author: gus19
 """
 
+#Create socket: s = socket.socket (socket_family, socket_type, protocol=0)
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
