833fff2e06554bac024ca1d14d33d22f8ecad28d
Gustavo Vicuña <gpvicuna@miuandes.cl>
2018-11-25 22:10:13 -0300

    Starting up

---
+++ client.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 22:08:34 2018
+
+@author: gus19
+"""
+
---
+++ server.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 22:07:48 2018
+
+@author: gus19
+"""
+
